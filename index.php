314801f153032228f40f2d278b055f11edaa7c41
Mannan4975 <142294231+Mannan4975@users.noreply.github.com>
2023-08-15 12:50:58 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>e-Krishi Clinic</title>
+    <style>
+        .tab-content {
+            display: none;
+        }
+    </style>
+</head>
+<body>
+    <div class="site-container">
+        <header>
+            <div class="title-bar">
+                <img src="pic1.jpg" alt="Agricultural Consultation Center" class="logo">
+                <h1>e-Krishi Clinic</h1>
+            </div>
+        </header>
+        <nav>
+            <ul class="navigation">
+                <li><a href="#about">About Us</a></li>
+                <li><a href="#services">Our Services</a></li>
+                <li><a href="#contact">Contact Us</a></li>
+            </ul>
+        </nav>
+        <main>
+		
+		
+		
+			<section id="default-content" class="default-content">
+            <div class="default-content-inner">
+                <h2>Transforming Agriculture Through Innovation</h2>
+                <p>Explore our services to achieve higher yields and sustainable growth.</p>
+                <a href="#services" class="cta-button">Learn More</a>
+            </div>
+			</section>
+            
+			
+			<section id="about" class="tab-content">
+            <div class="about-tabs">
+                <div class="about-tab" id="mission">
+                    <h3>Our Mission</h3>
+                    <p>We are committed to providing sustainable agricultural solutions.</p>
+                </div>
+                <div class="about-tab" id="values">
+                    <h3>Our Values</h3>
+                    <p>Integrity, innovation, and customer satisfaction are at the core of our business.</p>
+                </div>
+                <div class="about-tab" id="team">
+                    <h3>Our Team</h3>
+                    <p>Meet our dedicated and experienced team of agricultural experts.</p>
+                </div>
+                <div class="about-tab" id="history">
+                    <h3>Our History</h3>
+                    <p>From our humble beginnings to becoming a leader in agricultural consultation.</p>
+                </div>
+            </div>
+        </section>
+			
+			
+			
+			
+			
+			
+			
+			
+          <section id="services" class="tab-content">
+            <h2 style="text-align:center; margin-top:15px;">Our Services</h2>
+            <div class="service-tabs">
+                <div class="service-tab" id="crop-consulting">
+                    <h3>Crop Consulting</h3>
+                    <p>Maximizing your yield with expert advice.</p>
+                </div>
+                <div class="service-tab" id="soil-analysis">
+                    <h3>Soil Analysis</h3>
+                    <p>Understanding your soil for optimal growth.</p>
+                </div>
+                <div class="service-tab" id="pest-control">
+                    <h3>Pest Control Strategies</h3>
+                    <p>Protecting your crops from unwanted invaders.</p>
+                </div>
+                <div class="service-tab" id="irrigation-management">
+                    <h3>Irrigation Management</h3>
+                    <p>Efficiently managing water for healthy plants.</p>
+                </div>
+            </div>
+        </section>
+            
+			
+		
+
+		<section id="contact" class="tab-content">
+					<div class="contact-tabs">
+						<div class="contact-tab" id="email">
+							<h3>Email Us</h3>
+							<p>Send us your inquiries at <a href="mailto:info@ekrishiclinic.com">info@ekrishiclinic.com</a>.</p>
+						</div>
+						<div class="contact-tab" id="phone">
+							<h3>Call Us</h3>
+							<p>Contact our support team at +017******** for assistance.</p>
+						</div>
+						<div class="contact-tab" id="address">
+							<h3>Visit Us</h3>
+							<p>Come and meet us at SAU, Dhaka, Bangladesh.</p>
+						</div>
+						<div class="contact-tab" id="social">
+							<h3>Follow Us</h3>
+							<p>Stay connected with us on our <a href="https://www.facebook.com/ekrishiclinic">Facebook</a> page</p>
+						</div>
+					</div>
+				</section>		
+			
+			
+			
+			
+			
+        </main>
+        <footer>
+            <p>&copy; 2023 e-Krishi Clinic. All rights reserved.</p>
+        </footer>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
+
